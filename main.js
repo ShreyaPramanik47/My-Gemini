234b7e0d0073a4ea08f98e75dfb2c8d7111182be
ShreyaPramanik47 <shreyapramanik909@gmail.com>
2024-09-25 17:45:16 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('My Gemini!')
+  })
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+// const prompt = "What is dog.";
+
+const generate = async(prompt) =>{
+    try{
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+        return result.response.text();
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+app.get('/api/content',async(req,res)=>{
+    try{
+       const data =req.body.question;
+       const result = await generate(data);
+       res.send({
+        "result":result
+       })
+    }
+    catch(err){
+        res.send("error: "+ err)
+    }
+})
+// generate();
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
